4cad5193ca072f2fce4d0a3183389b01788c485a
FaV0RiT77 <fav0rit8825@gmail.com>
2023-10-30 17:24:27 +0400

    added phone and smscode fragments w/ their layouts

--- app/src/main/java/tennis/bot/mobile/onboarding/phone/PhoneInputFragment.kt
+++ app/src/main/java/tennis/bot/mobile/onboarding/phone/PhoneInputFragment.kt
@@ -1,6 +1,15 @@
 package tennis.bot.mobile.onboarding.phone
 
+import android.os.Bundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doOnTextChanged
+import com.google.android.material.textfield.TextInputLayout
 import dagger.hilt.android.AndroidEntryPoint
+import tennis.bot.mobile.R
 import tennis.bot.mobile.core.CoreFragment
 import tennis.bot.mobile.core.Inflation
 import tennis.bot.mobile.databinding.FragmentPhoneInputBinding
@@ -8,4 +17,36 @@ import tennis.bot.mobile.databinding.FragmentPhoneInputBinding
 @AndroidEntryPoint
 class PhoneInputFragment : CoreFragment<FragmentPhoneInputBinding>() {
     override val bindingInflation: Inflation<FragmentPhoneInputBinding> = FragmentPhoneInputBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        binding.phoneEt.addTextChangedListener {
+            if (it?.isEmpty() == true) {
+                binding.xIv.visibility = View.INVISIBLE
+            } else {
+                binding.xIv.visibility = View.VISIBLE
+            }
+        }
+        binding.phoneEt.addTextChangedListener(PhoneNumberFormattingTextWatcher("US"))
+        binding.phoneEt.doOnTextChanged { text, start, before, count ->
+            if (text!!.length < 14) {
+                binding.textInputLayout.error = "Введите действительный номер"
+            } else {
+                binding.textInputLayout.error = null
+            }
+        }
+        binding.xIv.setOnClickListener {
+            binding.phoneEt.setText("")
+        }
+
+        binding.buttonNext.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container_view, SmsCodeFragment())
+                .addToBackStack(SmsCodeFragment::class.java.name)
+                .commit()
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
---
+++ app/src/main/java/tennis/bot/mobile/onboarding/phone/SmsCodeFragment.kt
@@ -0,0 +1,95 @@
+package tennis.bot.mobile.onboarding.phone
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
+import dagger.hilt.android.AndroidEntryPoint
+import tennis.bot.mobile.R
+import tennis.bot.mobile.core.CoreFragment
+import tennis.bot.mobile.core.Inflation
+import tennis.bot.mobile.databinding.FragmentSmsCodeBinding
+import kotlin.math.min
+
+
+@AndroidEntryPoint
+class SmsCodeFragment : CoreFragment<FragmentSmsCodeBinding>() {
+    override val bindingInflation: Inflation<FragmentSmsCodeBinding> = FragmentSmsCodeBinding::inflate
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val listTv = listOf(binding.num1, binding.num2, binding.num3, binding.num4)
+        val listLines = listOf(binding.line1, binding.line2, binding.line3, binding.line4)
+        val correctCode = "1234"
+
+        fun EditText.showKeyboard(activity: Activity) {
+            val inputMethodManager = activity.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            requestFocus()
+            inputMethodManager.showSoftInput(this, 0)
+        }
+        binding.codeEt.showKeyboard(requireActivity())
+
+        binding.codeEt.addTextChangedListener {
+            val inputText = binding.codeEt.text.toString()
+            val numbers = ArrayList<String>(4)
+            inputText.forEach { numbers.add(it.toString()) }
+            displayNumbersInTextViews(numbers, listTv, listLines)
+            if (numbers.size < 4 || correctCode != inputText) {
+                for (i in 0 until 4) {
+                    listLines[i].backgroundTintList =
+                        ContextCompat.getColorStateList(requireContext(), androidx.appcompat.R.color.error_color_material_light);
+                    clearDeletedNumbers(listTv, 4, numbers)
+                }
+                binding.errorTv.visibility = View.VISIBLE
+            } else {
+                binding.errorTv.visibility = View.INVISIBLE
+            }
+
+        }
+
+        object : CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val remaining = millisUntilFinished / 1000
+                if (remaining < 10) {
+                    binding.timerTv.text = "0:0$remaining"
+                } else {
+                    binding.timerTv.text = "0:$remaining"
+                }
+            }
+
+            override fun onFinish() {
+                binding.timerTv.visibility = View.GONE
+            }
+        }.start()
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private fun displayNumbersInTextViews(
+        numbers: List<String>,
+        textViews: List<TextView>,
+        listLines: List<View>
+    ) {
+        val minSize = min(numbers.size, textViews.size)
+
+        for (i in 0 until minSize) {
+            textViews[i].text = numbers[i]
+            listLines[i].backgroundTintList =
+                ContextCompat.getColorStateList(requireContext(), R.color.tb_primary_green);
+        }
+    }
+
+    private fun clearDeletedNumbers(textViews: List<TextView>, oldNumbers: Int, newNumbers: List<String>) {
+        for (i in 0 until min(oldNumbers, textViews.size)) {
+            val textView = textViews[i]
+            if (!newNumbers.indices.contains(i)) {
+                textView.text = " " // Clear the text if the number is deleted
+            }
+        }
+    }
+}
