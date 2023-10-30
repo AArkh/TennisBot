3cfcd8fc8e16362631cc4e7475ff6f7b022586c1
FaV0RiT77 <fav0rit8825@gmail.com>
2023-10-30 21:56:13 +0400

    added phone and smscode fragments w/ their layouts. Created BottomSheetDialog for PhoneInputFragment, Adapter for it's RecyclerView and passed dummy data to it to see if works

---
+++ app/src/main/java/tennis/bot/mobile/onboarding/phone/PhoneInputAdapter.kt
@@ -0,0 +1,36 @@
+package tennis.bot.mobile.onboarding.phone
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.DrawableRes
+import androidx.recyclerview.widget.RecyclerView
+import tennis.bot.mobile.core.CoreAdapter
+import tennis.bot.mobile.databinding.RecyclerCountryItemBinding
+import javax.inject.Inject
+
+class PhoneInputAdapter @Inject constructor() : CoreAdapter<CountryCodeItemViewHolder>() {
+    override fun onBindViewHolder(holder: CountryCodeItemViewHolder, item: Any) {
+        val countryItem = item as? CountryItem ?: throw IllegalArgumentException("Item must be CountryItem")
+        holder.binding.countryIconIv.setImageResource(countryItem.icon)
+        holder.binding.countryNameTv.text = countryItem.countryName
+        holder.binding.countryCodeTv.text = countryItem.countryCode
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryCodeItemViewHolder {
+        val binding = RecyclerCountryItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return CountryCodeItemViewHolder(binding)
+    }
+
+}
+
+class CountryCodeItemViewHolder(
+    val binding: RecyclerCountryItemBinding
+) : RecyclerView.ViewHolder(binding.root)
+
+data class CountryItem(
+    val icon: Int,
+    val countryName: String,
+    val countryCode: String
+)
